5d4230e834f372149d46aab799b5443716fc8295
yunusga <yunusga@yandex.ru>
2017-07-22 12:49:55 +0500

    Новый модуль авторизации

--- bin/kakadu.js
+++ bin/kakadu.js
@@ -28,8 +28,6 @@ const bs              = require('browser-sync').create();
 const program         = require('commander');
 const chalk           = require('chalk');
 const pkg             = require('../package.json');
-
-const password        = require('generate-password').generate({length: 7, numbers: true});
 const getAuthParams   = (params) => params.split('@');
 
 let config = {};
@@ -37,27 +35,25 @@ let config = {};
 
 program
     .version(pkg.version)
-    .option('-a, --auth [user@password]', `set user@password for authorization [${pkg.name}@${password}]`, `${pkg.name}@${password}`)
+    .option('-a, --auth [user@password]', `set user@password for authorization`, pkg.name)
     .option('--proxy [url]', 'URL for proxy')
     .option('--port <n>', 'port for proxy', 7200)
     .option('--tech [tech]', 'tech for styles pre-processor (styl, scss, less)', /^(styl|scss|less)$/i, 'styl')
     .option('--nano', 'enable cssnano')
     .parse(process.argv);
 
+
 /**
  * Проверка правильности установки логина и пароля для авторизации
  */
-if (program.auth && getAuthParams(program.auth).length !== 2) {
-    console.log(`\n ${chalk.bold.yellow(pkg.name.toUpperCase())} запущен в режиме авторизации\n неправильно установлены ${chalk.bold.yellow('user@password')}`);
-    console.log(`\n ${chalk.bold.green('наберите:')} ${pkg.name} --help для справки`);
-    process.exit(1);
-} else if (program.auth && getAuthParams(program.auth).length === 2) {
-    console.log(`\n ${chalk.bold.yellow(pkg.name.toUpperCase())} запущен в режиме авторизации`);
-    console.log(chalk.gray('-------------------------------------'));
-    console.log(` ${chalk.bold.green(' логин:')} ${getAuthParams(program.auth)[0]}`);
-    console.log(` ${chalk.bold.green('пароль:')} ${getAuthParams(program.auth)[1]}`);
-    console.log(chalk.gray('-------------------------------------\n'));
+if (program.auth) {
+
+    bs.use(require('bs-auth'), {
+        user: getAuthParams(program.auth)[0],
+        pass: getAuthParams(program.auth)[1]
+    });
 }
+
 
 
 const stylePreProcessor = (tech) => {
@@ -153,31 +149,7 @@ const init = () => {
 
         if (exist) {
 
-            let authMiddleware = (req, res, next) => {
-                    
-                let auth = basicAuth(req);
-
-                if (auth && auth.name === getAuthParams(program.auth)[0] && auth.pass === getAuthParams(program.auth)[1]) {
-                    next();
-                } else {
-                    res.statusCode = 401;
-                    res.setHeader('WWW-Authenticate', 'Basic realm="KAKADU Static Server"');
-                    res.end('Access denied');
-                }
-            },
-            middlewares = [];
-
-            if (program.auth && getAuthParams(program.auth).length === 2) {
-                middlewares.push(authMiddleware);
-            }
-
             config = require(process.cwd() + '/config.js');
-
-            if (config.bs.hasOwnProperty('middleware')) {
-                middlewares.push(config.bs.middleware);
-            }
-
-            config.bs.middleware = middlewares;
 
             gulp.start('start');
 
@@ -185,7 +157,7 @@ const init = () => {
 
             gulp.start('copy-boilerplate');
         }
-        
+
     });
 }
 
